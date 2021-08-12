e0bef3b956048f90a4953e711291f642b3446abf
Haticenurcoskunn <70744158+Haticenurcoskunn@users.noreply.github.com>
2021-08-12 09:24:59 +0300

    changed by Sadık

--- deprem_app/lib/deprem/deprem.dart
+++ deprem_app/lib/deprem/deprem.dart
@@ -86,7 +86,6 @@ class _EarthQuakePageState extends State<EarthQuakePage> {
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
-            // By default, show a loading spinner
             return CircularProgressIndicator();
           },
         ),
